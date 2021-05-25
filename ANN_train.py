0719a157b3bc158aad24aea114b04db880a90d96
Yu Chen <71476156+kaede-cycy@users.noreply.github.com>
2021-05-25 12:09:42 +0900

    Create ANN_train.py

---
+++ ANN_train.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+# torch.manual_seed(1)    # reproducible
+
+#x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor), shape=(100, 1)
+#y = x.pow(2) + 0.2*torch.rand(x.size())                 # noisy y data (tensor), shape=(100, 1)
+x = torch.tensor([[0, 0.01, 0.02, 0.04, 0.07, 0.36, 0.48, 0.67, 0.74, 0.82, 0.84, 0.87, 0.9, 0.92, 0.93, 0.94, 0.96, 0.98, 1.01, 1.04, 1.07, 1.09]])    #make_features(random)
+y = torch.tensor([[0, 104.536, 205.358, 339.182, 512.934, 556.776, 556.262, 555.482, 559.038, 591.278, 610.962, 642.774, 691.644, 733.89, 751.794, 769.72, 792.654, 822.822, 864.076, 899.674, 939.16, 966.164]])
+x = x.t()
+y = y.t()
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
+        self.predict = torch.nn.Linear(n_hidden, n_output)   # output layer
+
+    def forward(self, x):
+        x = F.relu(self.hidden(x))      # activation function for hidden layer
+        x = self.predict(x)             # linear output
+        return x
+
+net = Net(n_feature=1, n_hidden=10, n_output=1)     # define the network
+print(net)  # net architecture
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.2)
+loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
+
+plt.ion()   # something about plotting
+
+for t in range(200):
+    prediction = net(x)     # input x and predict based on x
+
+    loss = loss_func(prediction, y)     # must be (1. nn output, 2. target)
+
+    optimizer.zero_grad()   # clear gradients for next train
+    loss.backward()         # backpropagation, compute gradients
+    optimizer.step()        # apply gradients
+
+    if t % 5 == 0:
+        # plot and show learning process
+        plt.cla()
+        plt.scatter(x.data.numpy(), y.data.numpy())
+        plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+        plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color':  'red'})
+        plt.pause(0.1)
+
+plt.ioff()
+plt.show()
