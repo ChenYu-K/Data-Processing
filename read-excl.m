af4856a3777644425d6e89cd871e62fabdfb53b0
Yu Chen <71476156+kaede-cycy@users.noreply.github.com>
2021-05-26 10:13:37 +0900

    Update read-excl.m

--- read-excl.m
+++ read-excl.m
@@ -1,5 +1,5 @@
 clear
-db=ls;
+db=ls;   %ファイル内の内容を読み込む
 db1=string(db);
 db1=deblank(db1);
 data=db1([3:50],1);
@@ -7,8 +7,8 @@ l=length(data);
 for i=1:l
    test_data(i,1)={data(i,1)};
     name=['a=csvread(''',data(i,1),''',20,1);'];
-    name1=strcat(name(1,1),name(1,2),name(1,3));
-    eval(name1);
+    name1=strcat(name(1,1),name(1,2),name(1,3)); #名前を分ける
+    eval(name1);  %名前を合併する
     slipdata=strsplit(test_data{i,1},'.');
     slipdata1(i,1)={slipdata(1,1)};
     slipdata1(i,2)={a};
