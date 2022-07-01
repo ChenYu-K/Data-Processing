2488a63892899693208b8c3be6f6da02a3e98b37
Yu Chen <71476156+ChenYu-K@users.noreply.github.com>
2022-07-01 22:39:45 +0900

    Update mp4toavi.m

--- DIC/mp4toavi.m
+++ DIC/mp4toavi.m
@@ -1,12 +1,20 @@
-reader = VideoReader('C0072.MP4');
-writer = VideoWriter('transcoded_C0072.avi', ...
-                        'Uncompressed AVI');
+reader = VideoReader('C0079.MP4');
+writer = VideoWriter('transcoded_C0072_test.avi', ...
+                        'Grayscale AVI');
 
+% Uncompressed (https://ww2.mathworks.cn/help/matlab/ref/videowriter.html?searchHighlight=videowriter&s_tid=srchtitle_videowriter_1#d123e1491750) option
 writer.FrameRate = reader.FrameRate;
+%writer.FrameCount = 3;
+frame = 1;
 
 open(writer);
 while hasFrame(reader)
-   img = readFrame(reader);
-   writeVideo(writer,img);
+   %if mod(frame,8) == 0 
+    img = readFrame(reader);
+    img_gray = rgb2gray(img);
+    writeVideo(writer,img_gray);
+   %end
+   frame = frame + 1;
 end
+
 close(writer);
